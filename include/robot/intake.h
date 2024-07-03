b44064e8c51a0afc75439b2e2d59dc8280ae94f2
Jay9971 <robertmasked@gmail.com>
2024-07-03 10:44:08 -0700

    intake_latch

--- include/robot/intake.h
+++ include/robot/intake.h
@@ -10,12 +10,18 @@ namespace Robot {
         /**
          * @brief Runs the main function of the intake system.
          * 
-         * This function takes user input to control the intake system.
+         * Takes user input to control the direction of the intake system.
          */
         void run(unsigned short direction);
 
+        /**
+         * @brief Outakes rings and scores them inside of latched mobile goal.
+         */
         void score();
 
+        /**
+         * @brief Controls the latching mechanism of the latch class.
+         */
         void latch();
     };
 }
---
+++ include/robot/latch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Robot {
+
+    /**
+     * @brief The Latch class represents a latching mechanism.
+     */
+    class Latch {
+        public:
+            /**
+             * @brief Runs latching mechanism.
+             */
+            void run();
+
+
+    };
+}
