301d9a0664278ade6c6cc9d03d19380607850532
Aswath Subramanian <aswsub123@gmail.com>
2024-09-28 16:53:29 -0700

    hotfix(globals): Motor ports updated + reset PID values

--- src/globals.cpp
+++ src/globals.cpp
@@ -18,8 +18,8 @@ namespace Globals {
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::Motor RightFront(6, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
-pros::Motor LeftFront(-5, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
-pros::Motor LeftBack(-7, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor LeftFront(-20, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor LeftBack(-10, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
 pros::Motor RightBack(8, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
 pros::Motor LeftMid(2, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
 pros::Motor RightMid(-3, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
@@ -46,7 +46,7 @@ pros::MotorGroup drive_({LeftFront.get_port(), RightFront.get_port(), LeftMid.ge
 lemlib::Drivetrain drivetrain{
     &drive_left,  // left drivetrain motors
     &drive_right, // right drivetrain motors
-    12.125,       // track width
+    12.25,       // track width
     lemlib::Omniwheel::OLD_325,
     450, // drivetrain rpm is 450
     2    // horizontal drift is 2
@@ -61,40 +61,39 @@ lemlib::OdomSensors sensors{
 };
 
 // forward/backward PID
-lemlib::ControllerSettings lateral_controller{
-    7.6, // kP
-    0,   // KI
-    8,   // kD
-    3,   // Anti Windup
-    1,   // smallErrorRange
-    100, // smallErrorTimeout
-    3,   // largeErrorRange
-    500, // largeErrorTimeout
-    90   // slew rate
-};
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
 
-// turning PID
-lemlib::ControllerSettings angular_controller{
-    4.04, // kP
-    0,    // kI
-    24.5, // kD
-    3,    // Anti Windup
-    1,    // smallErrorRange
-    100,  // smallErrorTimeout
-    3,    // largeErrorRange
-    500,  // largeErrorTimeout
-    0     // slew rate
-};
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
 
-lemlib::ExpoDriveCurve throttle_curve(3,    // joystick deadband out of 127
-                                      10,   // minimum output where drivetrain will move out of 127
-                                      1.019 // expo curve gain
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
 );
 
 // input curve for steer input during driver control
-lemlib::ExpoDriveCurve steer_curve(4,     // joystick deadband out of 127
-                                   6,     // minimum output where drivetrain will move out of 127
-                                   1.0053 // expo curve gain
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
 );
 
 lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sensors, &throttle_curve, &steer_curve);
